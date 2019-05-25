2f3b37a1cf6cd4973ed46b955eae67165e3d7bbf
N <46265532+NDom23@users.noreply.github.com>
2019-05-25 11:02:17 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+var playPause = anime({
+    // Target the red box
+    targets: 'div.box',
+    translateY: [
+//        How far and for how long in milliseconds
+//  One to go up and one to go down 
+        { value: 200, duration: 500},
+        { value: 0, duration: 800 }
+    ],
+    rotate:{
+        value: '1turn',
+        easing: 'easeInOutSine'
+    },
+    delay: function(el, i, l){ return i * 1000},
+    autoplay:false,
+    loop:true
+});
+
+document.querySelector('#boxes .play').onclick = playPause.play;
+
+document.querySelector('#boxes .pause').onclick = playPause.pause;
